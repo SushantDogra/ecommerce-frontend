3a4b568381da7fccdb25314a16b7fb72d8d647af
SushantDogra <sushantsingh.dogra@gmail.com>
2022-04-14 23:23:31 +0530

    created a slider and some dummy data

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://i.ibb.co/DQXJ78h/Screenshot-2022-04-14-at-10-01-26-PM.png",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/DG69bQ4/2.png",
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/cXFnLLV/3.png",
+    title: "LOUNGEWEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
