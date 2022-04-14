643af178c98e2fa1bf7a25eccde1240501099a37
SushantDogra <sushantsingh.dogra@gmail.com>
2022-04-14 21:21:00 +0530

    creating navbar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,88 @@
-import React from "react";
+import { Badge } from "@material-ui/core";
+import { Search, ShoppingCartOutlined } from "@material-ui/icons";
 import styled from "styled-components";
 
 const Container = styled.div`
   height: 60px;
-  background-color: black;
-  color: white;
+`;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+const Language = styled.span`
+  font-size: 14 px;
+  cursor: pointer;
+`;
+
+const SearchContainer = styled.span`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Input = styled.input`
+  border: none;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
 `;
 
 const NavBar = () => {
-  return <Container>navbar</Container>;
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input></Input>
+            <Search style={{ color: "Gray", fontSize: 16 }}></Search>
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>Dogra.</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary"></Badge>
+            <ShoppingCartOutlined></ShoppingCartOutlined>
+          </MenuItem>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default NavBar;
