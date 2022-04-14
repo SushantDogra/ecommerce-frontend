a4d40392174af9cd05a23f270945725a4e0e3868
SushantDogra <sushantsingh.dogra@gmail.com>
2022-04-14 20:19:00 +0530

    initialize home page and navbar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 60px;
+  background-color: black;
+  color: white;
+`;
+
+const NavBar = () => {
+  return <Container>navbar</Container>;
+};
+
+export default NavBar;
