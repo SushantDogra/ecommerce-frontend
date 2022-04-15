a23e95544deff3a7819365835bb1eec6272e30fd
SushantDogra <sushantsingh.dogra@gmail.com>
2022-04-15 11:50:53 +0530

    product list and product page

---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,78 @@
+import styled from "styled-components";
+import Announcement from "../components/Announcement";
+import Footer from "../components/Footer";
+import NavBar from "../components/NavBar";
+import NewsLetter from "../components/NewsLetter";
+import Products from "../components/Products";
+
+const Container = styled.div``;
+
+const Title = styled.h1`
+  margin: 20px;
+`;
+
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Filter = styled.div`
+  margin: 20px;
+`;
+
+const FilterText = styled.span`
+  font-size: 20px;
+  font-weight: 600;
+  margin-right: 20px;
+`;
+
+const Select = styled.select`
+  padding: 10px;
+  margin-right: 20px;
+`;
+const Option = styled.option``;
+
+const ProductList = () => {
+  return (
+    <Container>
+      <NavBar></NavBar>
+      <Announcement></Announcement>
+      <Title>Dresses</Title>
+      <FilterContainer>
+        <Filter>
+          <FilterText>Filter Products:</FilterText>
+          <Select name="color">
+            <Option disabled>Color</Option>
+            <Option>white</Option>
+            <Option>black</Option>
+            <Option>red</Option>
+            <Option>blue</Option>
+            <Option>yellow</Option>
+            <Option>green</Option>
+          </Select>
+          <Select name="size">
+            <Option disabled>Size</Option>
+            <Option>XS</Option>
+            <Option>S</Option>
+            <Option>M</Option>
+            <Option>L</Option>
+            <Option>XL</Option>
+          </Select>
+        </Filter>
+        <Filter>
+          <FilterText>Sort Products:</FilterText>
+          <Select>
+            <Option value="newest">Newest</Option>
+            <Option value="asc">Price (asc)</Option>
+            <Option value="desc">Price (desc)</Option>
+          </Select>
+        </Filter>
+      </FilterContainer>
+      <Products></Products>
+      <NewsLetter></NewsLetter>
+      <Footer> </Footer>
+    </Container>
+  );
+};
+
+export default ProductList;
