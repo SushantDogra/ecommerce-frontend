626e4962cb4d9af00ccb4fb759c5222383698e7f
SushantDogra <sushantsingh.dogra@gmail.com>
2022-04-15 15:49:13 +0530

    making the UI compatible with mobile

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,19 @@
 import { Badge } from "@material-ui/core";
 import { Search, ShoppingCartOutlined } from "@material-ui/icons";
 import styled from "styled-components";
+import { mobile } from "../responsive";
 
 const Container = styled.div`
   height: 60px;
+  ${mobile({ height: "50px" })}
 `;
 
 const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
-  justify-content: space-between;
   align-items: center;
+  justify-content: space-between;
+  ${mobile({ padding: "10px 0px" })}
 `;
 
 const Left = styled.div`
@@ -20,11 +23,12 @@ const Left = styled.div`
 `;
 
 const Language = styled.span`
-  font-size: 14 px;
+  font-size: 14px;
   cursor: pointer;
+  ${mobile({ display: "none" })}
 `;
 
-const SearchContainer = styled.span`
+const SearchContainer = styled.div`
   border: 0.5px solid lightgray;
   display: flex;
   align-items: center;
@@ -34,6 +38,7 @@ const SearchContainer = styled.span`
 
 const Input = styled.input`
   border: none;
+  ${mobile({ width: "50px" })}
 `;
 
 const Center = styled.div`
@@ -43,19 +48,21 @@ const Center = styled.div`
 
 const Logo = styled.h1`
   font-weight: bold;
+  ${mobile({ fontSize: "18px" })}
 `;
-
 const Right = styled.div`
   flex: 1;
   display: flex;
   align-items: center;
   justify-content: flex-end;
+  ${mobile({ flex: 2, justifyContent: "center" })}
 `;
 
 const MenuItem = styled.div`
   font-size: 14px;
   cursor: pointer;
   margin-left: 25px;
+  ${mobile({ fontSize: "12px", marginLeft: "10px" })}
 `;
 
 const NavBar = () => {
@@ -65,7 +72,7 @@ const NavBar = () => {
         <Left>
           <Language>EN</Language>
           <SearchContainer>
-            <Input></Input>
+            <Input placeholder="Search"></Input>
             <Search style={{ color: "Gray", fontSize: 16 }}></Search>
           </SearchContainer>
         </Left>
