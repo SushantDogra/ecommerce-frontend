904efe8ad05b6c1615a81daf69ac9566f8fff1d3
SushantDogra <sushantsingh.dogra@gmail.com>
2022-04-17 15:59:16 +0530

    initializing router

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,38 @@ import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 import Cart from "./pages/Cart";
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+
 const App = () => {
-  return <Home></Home>;
-  // return <ProductList></ProductList>;
-  // return <Product></Product>;
-  // return <Register></Register>;
-  // return <Login></Login>;
-  // return <Cart></Cart>;
+  const user = false;
+
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/products/:category">
+          <ProductList />
+        </Route>
+        <Route path="/product/:id">
+          <Product />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
+        <Route path="/register">
+          {user ? <Redirect to="/" /> : <Register />}
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
