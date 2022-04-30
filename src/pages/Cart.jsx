8f1a2f2bdc115f63c2ff26d5ac1cd7cfefdb2b07
SushantDogra <sushantsingh.dogra@gmail.com>
2022-04-30 15:27:45 +0530

    bugfix

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -249,7 +249,7 @@ const Cart = () => {
               <SummaryItemPrice>$ {cart.total}</SummaryItemPrice>
             </SummaryItem>
             <StripeCheckout
-              name="Lama Shop"
+              name="The Dogra Shop"
               image="https://avatars.githubusercontent.com/u/1486366?v=4"
               billingAddress
               shippingAddress
